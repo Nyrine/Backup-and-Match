1f2823551a2b69e7ddd05ccee88c0414da425c9b
Katniss Everdeen <jan.bentsche@googlemail.com>
2019-02-06 22:20:16 +0100

    FirstPush

---
+++ Backup and Match/main.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.FileListDataSet = New Backup_and_Match.FileListDataSet()
+        Me.FileListDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FileListDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FileListDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 466)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "OpenXML File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.DataSource = Me.FileListDataSetBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 23
+        Me.DataGridView1.Size = New System.Drawing.Size(874, 448)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'FileListDataSet
+        '
+        Me.FileListDataSet.DataSetName = "FileListDataSet"
+        Me.FileListDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'FileListDataSetBindingSource
+        '
+        Me.FileListDataSetBindingSource.DataSource = Me.FileListDataSet
+        Me.FileListDataSetBindingSource.Position = 0
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(898, 501)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "main"
+        Me.ShowInTaskbar = False
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FileListDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FileListDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents FileListDataSetBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents FileListDataSet As Backup_and_Match.FileListDataSet
+
+End Class
---
+++ Backup and Match/main.vb
@@ -0,0 +1,46 @@
+﻿Imports SearchIndexer
+Imports System.Xml
+
+
+Public Class main
+    Public XMLFile As String
+    Private Sub main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim sind As New sindex
+
+        sind.Show()
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim opF As New OpenFileDialog
+        With opF
+            .ShowDialog()
+            XMLFile = .FileName
+        End With
+        Dim xmlDoc As New XmlDataDocument
+        xmlDoc.DataSet.ReadXml(XMLFile)
+        DataGridView1.DataSource = xmlDoc.DataSet.Tables
+    End Sub
+    Private Sub LoadBranches()
+
+        Dim filePath As String = (XMLFile)
+        Dim BranchDS As New DataSet()
+        BranchDS.ReadXml(filePath)
+        Try
+            With DataGridView1 ' the datagridview
+                .DataSource = BranchDS
+                .DataMember = "branch"
+                .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+                .AutoSize = True
+                .EditMode = DataGridViewEditMode.EditOnEnter
+            End With
+        Catch ex As Exception
+            MsgBox("Branch XML File Could Not Be Loaded: " & ex.Message)
+        End Try
+
+    End Sub
+
+End Class
